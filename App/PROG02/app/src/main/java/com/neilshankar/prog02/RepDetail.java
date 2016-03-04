81c55638613ab8a1bf92e7cf34f54c64d7e0570b
Neil Shankar <neil.s.shankar@gmail.com>
2016-03-03 18:31:37 -0800

    mobile xml frameworks built

---
+++ App/PROG02/app/src/main/java/com/neilshankar/prog02/RepDetail.java
@@ -0,0 +1,13 @@
+package com.neilshankar.prog02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rep_detail);
+    }
+}
