2420543cc72f3437fadb5b31eb85b697b73fb203
Neil Shankar <neil.s.shankar@gmail.com>
2016-03-03 19:13:12 -0800

    rep list updates based on location

--- App/PROG02/app/src/main/java/com/neilshankar/prog02/RepList.java
+++ App/PROG02/app/src/main/java/com/neilshankar/prog02/RepList.java
@@ -1,7 +1,10 @@
 package com.neilshankar.prog02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class RepList extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class RepList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rep_list);
+
+        Intent it = getIntent();
+        final int zip = Integer.parseInt(it.getStringExtra("zip"));
+
+        if (zip == 94704) {
+            ((ImageButton)findViewById(R.id.img1)).setImageResource(R.drawable.face1);
+            ((ImageButton)findViewById(R.id.img2)).setImageResource(R.drawable.face2);
+            ((ImageButton)findViewById(R.id.img3)).setImageResource(R.drawable.face3);
+            ((TextView)findViewById(R.id.name1)).setText("Brad Rigo (R)");
+            ((TextView)findViewById(R.id.name2)).setText("Pat Toomey (R)");
+            ((TextView)findViewById(R.id.name3)).setText("Chad McGraw (D)");
+        } else if (zip == 19403) {
+            ((ImageButton) findViewById(R.id.img1)).setImageResource(R.drawable.face4);
+            ((ImageButton) findViewById(R.id.img2)).setImageResource(R.drawable.face5);
+            ((ImageButton) findViewById(R.id.img3)).setImageResource(R.drawable.face6);
+            ((TextView) findViewById(R.id.name1)).setText("Hunter Wayans (D)");
+            ((TextView) findViewById(R.id.name2)).setText("Rain Bust (R)");
+            ((TextView) findViewById(R.id.name3)).setText("Cooper Lighthook (D)");
+        } else {
+            ((ImageButton)findViewById(R.id.img1)).setImageResource(R.drawable.face7);
+            ((ImageButton)findViewById(R.id.img2)).setImageResource(R.drawable.face8);
+            ((ImageButton)findViewById(R.id.img3)).setImageResource(R.drawable.face9);
+            ((TextView)findViewById(R.id.name1)).setText("Buster White (D)");
+            ((TextView)findViewById(R.id.name2)).setText("Nate Cream (R)");
+            ((TextView)findViewById(R.id.name3)).setText("Andrew Carmichael (D)");
+        }
     }
+
+
 }
