de05848226f48d7647b689776a7f7c7c7ed22890
Neil Shankar <neil.s.shankar@gmail.com>
2016-03-03 19:00:21 -0800

    location picker works

--- App/PROG02/app/src/main/java/com/neilshankar/prog02/EnterLocation.java
+++ App/PROG02/app/src/main/java/com/neilshankar/prog02/EnterLocation.java
@@ -1,7 +1,12 @@
 package com.neilshankar.prog02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EnterLocation extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class EnterLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_location);
+
+        Button use_my_location = (Button)findViewById(R.id.use_my_location);
+        use_my_location.setClickable(true);
+        use_my_location.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View clicked) {
+                ((TextView)findViewById(R.id.zip_input)).setText("94704");
+            }
+        });
+
+        Button enter = (Button)findViewById(R.id.enter);
+        enter.setClickable(true);
+        enter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View clicked) {
+                Intent it = new Intent(EnterLocation.this, RepList.class);
+                it.putExtra("zip", "" + ((EditText) findViewById(R.id.zip_input)).getText());
+                EnterLocation.this.startActivity(it);
+            }
+        });
     }
+
 }
