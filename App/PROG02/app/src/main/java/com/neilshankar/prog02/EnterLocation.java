12b5a2a2453126524b24a99e18a051861b20075c
Neil Shankar <neil.s.shankar@gmail.com>
2016-03-03 17:30:59 -0800

    styling xml's first, then java

---
+++ App/PROG02/app/src/main/java/com/neilshankar/prog02/EnterLocation.java
@@ -0,0 +1,13 @@
+package com.neilshankar.prog02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_location);
+    }
+}
