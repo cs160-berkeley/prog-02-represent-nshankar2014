c5823591209425dd4b49fb2cecdce2accb0228cc
Neil Shankar <neil.s.shankar@gmail.com>
2016-03-12 15:30:25 -0800

    need to parse http response

--- App/PROG2WW/mobile/src/main/java/com/neilshankar/prog02ww/EnterLocation.java
+++ App/PROG2WW/mobile/src/main/java/com/neilshankar/prog02ww/EnterLocation.java
@@ -1,34 +1,19 @@
 package com.neilshankar.prog02ww;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.wearable.Wearable;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class EnterLocation extends AppCompatActivity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -49,43 +34,42 @@ public class EnterLocation extends AppCompatActivity
 
         Button use_my_location = (Button)findViewById(R.id.use_my_location);
         use_my_location.setClickable(true);
+
         use_my_location.setOnClickListener(new View.OnClickListener() {
             public void onClick(View clicked) {
+            Intent it = new Intent(EnterLocation.this, RepList.class);
+            Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
 
             // get location
             if (getApplicationContext().checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION") == PackageManager.PERMISSION_GRANTED) {
-
-                String lat;
+                double lat = 0;
+                double lon = 0;
                 Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
                 if (mLastLocation != null) {
-                    lat = ""+mLastLocation.getLatitude();
-                }
-
-
-                String stringUrl = "http://congress.api.sunlightfoundation.com/legislators/locate?zip=94704&apikey=d5c6792487144ad397965ccbe5cb713d";
-                ConnectivityManager connMgr = (ConnectivityManager)
-                        getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                if (networkInfo != null && networkInfo.isConnected()) {
-                    new DownloadWebpageTask().execute(stringUrl);
+                    lat = mLastLocation.getLatitude();
+                    lon = mLastLocation.getLongitude();
                 } else {
-                    ((EditText) findViewById(R.id.zip_input)).setText("000000");
+                    lat = 500;
+                    lon = 500;
                 }
-
-
-
-
-
+                it.putExtra("zip", "");
+                it.putExtra("lat", ""+lat);
+                it.putExtra("lon", ""+lon);
+                sendIntent.putExtra("zip", "");
+                sendIntent.putExtra("lat", ""+lat);
+                sendIntent.putExtra("lon", ""+lon);
             } else {
                 // fine location permission disabled... force 94704
-                Intent it = new Intent(EnterLocation.this, RepList.class);
                 it.putExtra("zip", "94704");
-                EnterLocation.this.startActivity(it);
-
-                Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
+                it.putExtra("lat", "");
+                it.putExtra("lon", "");
                 sendIntent.putExtra("zip", "94704");
-                startService(sendIntent);
+                sendIntent.putExtra("lat", "");
+                sendIntent.putExtra("lon", "");
             }
+
+            EnterLocation.this.startActivity(it);
+            startService(sendIntent);
             }
         });
 
@@ -94,57 +78,19 @@ public class EnterLocation extends AppCompatActivity
         enter.setOnClickListener(new View.OnClickListener() {
             public void onClick(View clicked) {
                 Intent it = new Intent(EnterLocation.this, RepList.class);
-                it.putExtra("zip", "" + ((EditText) findViewById(R.id.zip_input)).getText());
-                EnterLocation.this.startActivity(it);
-
                 Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
-                sendIntent.putExtra("zip", "" + ((EditText) findViewById(R.id.zip_input)).getText());
+                String zip = "" + ((EditText) findViewById(R.id.zip_input)).getText();
+                it.putExtra("zip", zip);
+                it.putExtra("lat", "");
+                it.putExtra("lon", "");
+                sendIntent.putExtra("zip", zip);
+                sendIntent.putExtra("lat", "");
+                sendIntent.putExtra("lon", "");
+                EnterLocation.this.startActivity(it);
                 startService(sendIntent);
             }
         });
     }
-
-    public String getInternetData() throws Exception {
-
-        URL url = new URL("http://congress.api.sunlightfoundation.com/legislators/locate?zip=94704&apikey=d5c6792487144ad397965ccbe5cb713d");
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        BufferedReader in = null;
-        String data = "";
-
-        try {
-            // InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            // readStream(in);
-
-            in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            StringBuffer sb = new StringBuffer("");
-            String l = "";
-            String nl = System.getProperty("line.separator");
-            while ((l = in.readLine()) != null) {
-                sb.append(l + nl);
-            }
-            in.close();
-            urlConnection.disconnect();
-            data = sb.toString();
-            Log.d("0000000000000000", "data: " + data);
-            return data;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
-        } finally {
-//            urlConnection.disconnect();
-//            if (in != null) {
-//                try {
-//                    in.close();
-//                    return data;
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            } else {
-//                Log.d("0000000000000000", "in == null");
-//            }
-        }
-    }
-
 
     @Override
     protected void onResume() {
@@ -168,73 +114,19 @@ public class EnterLocation extends AppCompatActivity
     public void onConnectionFailed(ConnectionResult connResult) {}
 
 
-    // shoutout to http://bit.ly/1XlKtX1
-    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
-    // URL string and uses it to create an HttpUrlConnection. Once the connection
-    // has been established, the AsyncTask downloads the contents of the webpage as
-    // an InputStream. Finally, the InputStream is converted into a string, which is
-    // displayed in the UI by the AsyncTask's onPostExecute method.
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            // params comes from the execute() call: params[0] is the url.
-            try {
-                return downloadUrl(urls[0]);
-            } catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid.";
-            }
-        }
-
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d("0000000000000000", "async results: " + result);
-        }
-    }
-
-    // Given a URL, establishes an HttpUrlConnection and retrieves
-    // the web page content as a InputStream, which it returns as
-    // a string.
-    private String downloadUrl(String myurl) throws IOException {
-
-        InputStream is = null;
-        int len = 500; // Only display the first 500 characters of the retrieved web page content.
-
-        try {
-            URL url = new URL(myurl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            // Starts the query
-            conn.connect();
-            int response = conn.getResponseCode();
-            Log.d("0000000000000000", "The response is: " + response);
-            is = conn.getInputStream();
-
-            // Convert the InputStream into a string
-            String contentAsString = readIt(is, len);
-            Log.d("0000000000000000", "contentAsString: " + contentAsString);
-            return contentAsString;
-
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        } finally {
-            if (is != null) {
-                is.close();
-            }
-        }
-    }
-
-    // Reads an InputStream and converts it to a String.
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[len];
-        reader.read(buffer);
-        return new String(buffer);
-    }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- App/PROG2WW/mobile/src/main/java/com/neilshankar/prog02ww/RepList.java
+++ App/PROG2WW/mobile/src/main/java/com/neilshankar/prog02ww/RepList.java
@@ -1,57 +1,52 @@
 package com.neilshankar.prog02ww;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RepList extends AppCompatActivity {
+
+    private Intent it;
+    private String zip;
+    private String lat;
+    private String lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rep_list);
 
-        Intent it = getIntent();
-        final int zip = Integer.parseInt(it.getStringExtra("zip"));
+        it = getIntent();
+        zip = it.getStringExtra("zip");
+        lat = it.getStringExtra("lat");
+        lon = it.getStringExtra("lon");
+        Log.d("0000000000000000", "zip: " + zip);
+        Log.d("0000000000000000", "lat: " + lat);
+        Log.d("0000000000000000", "lon: " + lon);
 
-        if (zip == 94704) {
-            ((ImageButton)findViewById(R.id.img1)).setImageResource(R.drawable.face1);
-            ((ImageButton)findViewById(R.id.img2)).setImageResource(R.drawable.face2);
-            ((ImageButton)findViewById(R.id.img3)).setImageResource(R.drawable.face3);
-            ((LinearLayout)findViewById(R.id.card1)).setTag("face1");
-            ((LinearLayout)findViewById(R.id.card2)).setTag("face2");
-            ((LinearLayout)findViewById(R.id.card3)).setTag("face3");
-            ((TextView)findViewById(R.id.name1)).setText("Brad Rigo (R)");
-            ((TextView)findViewById(R.id.name2)).setText("Pat Toomey (R)");
-            ((TextView)findViewById(R.id.name3)).setText("Chad McGraw (D)");
-        } else if (zip == 19403) {
-            ((ImageButton) findViewById(R.id.img1)).setImageResource(R.drawable.face4);
-            ((ImageButton) findViewById(R.id.img2)).setImageResource(R.drawable.face5);
-            ((ImageButton) findViewById(R.id.img3)).setImageResource(R.drawable.face6);
-            ((LinearLayout)findViewById(R.id.card1)).setTag("face4");
-            ((LinearLayout)findViewById(R.id.card2)).setTag("face5");
-            ((LinearLayout)findViewById(R.id.card3)).setTag("face6");
-            ((TextView) findViewById(R.id.name1)).setText("Hunter Wayans (D)");
-            ((TextView) findViewById(R.id.name2)).setText("Rain Bust (R)");
-            ((TextView) findViewById(R.id.name3)).setText("Cooper Lighthook (D)");
-        } else {
-            ((ImageButton)findViewById(R.id.img1)).setImageResource(R.drawable.face7);
-            ((ImageButton)findViewById(R.id.img2)).setImageResource(R.drawable.face8);
-            ((ImageButton)findViewById(R.id.img3)).setImageResource(R.drawable.face9);
-            ((LinearLayout)findViewById(R.id.card1)).setTag("face7");
-            ((LinearLayout)findViewById(R.id.card2)).setTag("face8");
-            ((LinearLayout)findViewById(R.id.card3)).setTag("face9");
-            ((TextView)findViewById(R.id.name1)).setText("Buster White (D)");
-            ((TextView)findViewById(R.id.name2)).setText("Nate Cream (R)");
-            ((TextView)findViewById(R.id.name3)).setText("Andrew Carmichael (D)");
-        }
+        attachClickListeners();
+        fetchData();
+    }
 
-        // attach a click listener to each ImageButton and Button
+    // attach a click listener to each ImageButton and Button
+    private void attachClickListeners() {
         LinearLayout full = (LinearLayout) findViewById(R.id.full);
         for (int i = 0; i < full.getChildCount(); i += 1) {
             View v = full.getChildAt(i);
@@ -92,13 +87,104 @@ public class RepList extends AppCompatActivity {
                 }
             }
         }
-
-
-
-
-
-
     }
 
+    // construct HTTP GET query with either (zip) or (lat and lon)
+    private void fetchData() {
+        String stringUrl = "http://congress.api.sunlightfoundation.com/legislators/locate?";
+        if (zip.equals("")) {
+            stringUrl += "zip=" + zip;
+        } else {
+            stringUrl += "latitude=" + lat + "&longitude=" + lon;
+        }
+        stringUrl += "&apikey=d5c6792487144ad397965ccbe5cb713d";
+        Log.d("0000000000000000", "stringUrl: " + stringUrl);
+
+        // make the GET request
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            Log.d("0000000000000000", "network contectivity issues");
+        }
+    }
+
+    // shoutout to http://bit.ly/1XlKtX1
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute calls fillLayout(), which fills the UI with the json info
+        @Override
+        protected void onPostExecute(String result) {
+            fillLayout(result);
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        int len = 500; // Only display the first 500 characters of the retrieved web page content.
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            //Log.d("0000000000000000", "The response is: " + conn.getResponseCode());
+
+            is = conn.getInputStream();
+            String contentAsString = readIt(is, len);
+            Log.d("0000000000000000", "contentAsString: " + contentAsString);
+            return contentAsString;
+
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    // fill layout with json info
+    private void fillLayout(String result) {
+        if (zip.equals("94704")) {
+            ((ImageButton)findViewById(R.id.img1)).setImageResource(R.drawable.face1);
+            ((ImageButton)findViewById(R.id.img2)).setImageResource(R.drawable.face2);
+            ((ImageButton)findViewById(R.id.img3)).setImageResource(R.drawable.face3);
+            ((LinearLayout)findViewById(R.id.card1)).setTag("face1");
+            ((LinearLayout)findViewById(R.id.card2)).setTag("face2");
+            ((LinearLayout)findViewById(R.id.card3)).setTag("face3");
+            ((TextView)findViewById(R.id.name1)).setText("Brad Rigo (R)");
+            ((TextView)findViewById(R.id.name2)).setText("Pat Toomey (R)");
+            ((TextView)findViewById(R.id.name3)).setText("Chad McGraw (D)");
+        }
+    }
 
 }
